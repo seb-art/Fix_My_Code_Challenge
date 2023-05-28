f59e323096ce0c2ed831e0a4a516c12db964d6ca
seb-art <kimanzikims300@gmail.com>
2023-05-28 12:50:14 -0700

    fizz fix

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -32,5 +32,9 @@ if __name__ == '__main__':
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
-        print("Example
+        print("Example: ./0-fizzbuzz.py 89")
+        sys.exit(1)
+
+    number = int(sys.argv[1])
+    fizzbuzz(number)
 
